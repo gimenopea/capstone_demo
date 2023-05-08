6ad348815ad0067449e1319e5dba023c1a9179b1
gimenopea <gimenopea@gmail.com>
2023-05-07 23:33:52 -0400

    update_wsgi_file

--- gimenopea_pa_wsgi.py
+++ gimenopea_pa_wsgi.py
@@ -1,9 +1,17 @@
 import sys
+import os
 
-# add your project directory to the sys.path
+# Add your project directory to the sys.path
 project_home = u'/home/gimenopea/capstone_demo'
+
+# Load environment variables from .env file
+from dotenv import load_dotenv
+
+project_folder = os.path.expanduser('~/capstone_demo')  # Adjust as appropriate
+load_dotenv(os.path.join(project_folder, '.env'))
+
 if project_home not in sys.path:
     sys.path = [project_home] + sys.path
 
-# import flask app but need to call it "application" for WSGI to work
+# Import Flask app, but need to call it "application" for WSGI to work
 from app import app as application  # noqa
